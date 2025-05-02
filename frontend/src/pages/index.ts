7eab8c5102aab260f8d1338a99cb12caa4c21d09
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-05-02 19:55:44 +0200

    fix: minor tweaks to barrel exports

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as About } from "./About";
-export { default as CreateTimetable } from "./CreateTimetable";
 export { default as Home } from "./Home";
+export { default as CreateTimetable } from "./CreateTimetable";
 export { default as ViewTimetable } from "./ViewTimetable";
