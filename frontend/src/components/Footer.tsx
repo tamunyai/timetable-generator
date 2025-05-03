94c2193f3ecd2b81f0fa65681f62cf24e0e39b89
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-05-03 20:52:49 +0200

    fix: improve `Footer` component ui
    - added `hover:underline` to main div
    - pushed footer content down for better visual hierachy

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-between items-center mx-auto px-4 py-6 border-t max-w-5xl text-gray-500 text-sm text-center">
-      <div className="mb-2">
+    <footer className="flex justify-between items-center mx-auto mt-24 px-4 py-6 border-t max-w-5xl text-sm text-center">
+      <div className="mb-2 hover:underline">
         <a
           href="https://github.com/tamunyai/timetable-generator"
           target="_blank"
@@ -23,7 +23,7 @@ const Footer: React.FC = () => {
             <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
             <path d="M9 18c-4.51 2-5-2-7-2" />
           </svg>
-          <span className="hover:underline">GitHub</span>
+          <span>GitHub</span>
         </a>
       </div>
       <p>&copy; {new Date().getFullYear()} Group 15. All rights reserved.</p>
