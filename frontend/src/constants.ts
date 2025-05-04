2ff36d328c37879258f5fbdafee0ea4f873b0359
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-05-04 14:00:46 +0200

    chore: define global constant env helper `isDev`
    - added a dedicated `env.ts` to track environment logic

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,8 +1,9 @@
+import { isDev } from "./env";
+
 export const LECTURERS_KEY = "ttg_lecturers";
 export const PROGRAMMES_KEY = "ttg_programmes";
 
-export const API_URL =
-  import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
+export const API_URL = isDev ? "http://127.0.0.1:5000/api" : "/api";
 
 export const CONTRIBUTORS: Contributor[] = [
   { name: "Kamogelo Selepe", role: "Project Lead" },
