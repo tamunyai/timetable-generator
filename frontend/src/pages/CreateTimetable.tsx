d33acb81108e7e4b544a7ff478411e64c9bdbd1b
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-05-04 13:40:43 +0200

    fix(ui): improve add module responsiveness

--- frontend/src/pages/CreateTimetable.tsx
+++ frontend/src/pages/CreateTimetable.tsx
@@ -403,21 +403,22 @@ const CreateTimetable = () => {
                         key={moduleIndex}
                         className="flex sm:flex-row flex-col sm:items-center gap-2 w-full"
                       >
-                        <input
-                          id={`module-${moduleIndex + 1}-name`}
-                          type="text"
-                          value={module.name}
-                          onChange={(e) =>
-                            updateModule(
-                              programmeIndex,
-                              moduleIndex,
-                              "name",
-                              e.target.value
-                            )
-                          }
-                          placeholder={`Module Name ${moduleIndex + 1}`}
-                          className="px-3 py-2 border w-full text-sm"
-                        />
+                        <div className="flex">
+                          <input
+                            id={`module-${moduleIndex + 1}-name`}
+                            type="text"
+                            value={module.name}
+                            onChange={(e) =>
+                              updateModule(
+                                programmeIndex,
+                                moduleIndex,
+                                "name",
+                                e.target.value
+                              )
+                            }
+                            placeholder={`Module Name ${moduleIndex + 1}`}
+                            className="px-3 py-2 border w-full text-sm"
+                          />
 
                           <Button
                             type="button"
@@ -468,53 +469,56 @@ const CreateTimetable = () => {
                             className="px-3 py-2 border w-full text-sm"
                           />
 
-                        <select
-                          id={`module-${moduleIndex + 1}-units`}
-                          value={module.units}
-                          onChange={(e) =>
-                            updateModule(
-                              programmeIndex,
-                              moduleIndex,
-                              "units",
-                              parseInt(e.target.value)
-                            )
-                          }
-                          className="px-3 py-2 border w-full text-sm"
-                        >
-                          <option value="">Units</option>
-                          <option value={2}>2</option>
-                          <option value={3}>3</option>
-                          <option value={4}>4</option>
-                        </select>
+                          <select
+                            id={`module-${moduleIndex + 1}-units`}
+                            value={module.units}
+                            onChange={(e) =>
+                              updateModule(
+                                programmeIndex,
+                                moduleIndex,
+                                "units",
+                                parseInt(e.target.value)
+                              )
+                            }
+                            className="px-3 py-2 border w-full text-gray-400 text-sm"
+                          >
+                            <option value="">Units</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                          </select>
+                        </div>
 
-                        <Button
-                          type="button"
-                          label={
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeWidth="2"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="w-4 h-4 lucide lucide-trash2-icon lucide-trash-2"
-                            >
-                              <path d="M3 6h18" />
-                              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                              <line x1="10" x2="10" y1="11" y2="17" />
-                              <line x1="14" x2="14" y1="11" y2="17" />
-                            </svg>
-                          }
-                          onClick={() =>
-                            removeModule(programmeIndex, moduleIndex)
-                          }
-                          className="border-0"
-                          disabled={programme.modules.length === 1}
-                        />
+                        <div className="hidden sm:block">
+                          <Button
+                            type="button"
+                            label={
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="w-4 h-4 lucide lucide-trash2-icon lucide-trash-2"
+                              >
+                                <path d="M3 6h18" />
+                                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                <line x1="10" x2="10" y1="11" y2="17" />
+                                <line x1="14" x2="14" y1="11" y2="17" />
+                              </svg>
+                            }
+                            onClick={() =>
+                              removeModule(programmeIndex, moduleIndex)
+                            }
+                            className="border-0"
+                            disabled={programme.modules.length === 1}
+                          />
+                        </div>
                       </div>
                     ))}
                   </div>
